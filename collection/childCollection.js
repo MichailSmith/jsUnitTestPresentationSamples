f40d5fd734d6fe5efa5dca782e2c53d513f3a091
Michail Smith <misha.bignife@gmail.com>
2015-01-29 14:58:47 -0600

    adds child collection test for url

--- collection/childCollection.js
+++ collection/childCollection.js
@@ -4,5 +4,12 @@ module.exports = backbone.Collection.extend({
     parse: function(response){
         this.totalCount = response.totalCount;
         return response.data;
+    },
+    initialize: function(options){
+        options = options || {};
+        this.parentId = options.parentId;
+    },
+    url: function(){
+        return 'api/parent/' + this.parentId + '/children';
     }
 });
--- collection/childCollectionTest.js
+++ collection/childCollectionTest.js
@@ -3,7 +3,7 @@ var ChildCollection = require('./childCollection');
 require('should');
 
 describe('ChildCollection', function(){
-    var model = new ChildCollection();
+    var model = new ChildCollection({parentId:5});
     describe('parse', function(){
         it('returns response.data', function(){
             model.parse({data:'sample data'}).should.equal('sample data');
@@ -11,6 +11,11 @@ describe('ChildCollection', function(){
         it('sets totalCount to response.totalCount', function(){
             model.parse({totalCount: 5});
             model.totalCount.should.equal(5);
+        });
+    });
+    describe('url', function(){
+        it('returns \'api/parent/:id/children\'', function(){
+           model.url().should.equal('api/parent/5/children');
         });
     });
 });
