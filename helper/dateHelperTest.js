c9e6adf9437b1ff72d6c696f3e2f3f048cbd603d
Michail Smith <misha.bignife@gmail.com>
2015-01-29 16:45:29 -0600

    adds missing semicolons to dateHelperTest

--- helper/dateHelperTest.js
+++ helper/dateHelperTest.js
@@ -5,11 +5,12 @@ describe('dateHelper', function(){
     describe('with date parameter', function(){
         it('should return date component of parameter', function(){
             dateHelper('1-1-2015 10:00:00').should.equal('1/1/2015');
-        })
-    })
+        });
+    });
+
     describe('with not-truthy parameter', function(){
         it('should return not-truthy result', function(){
             should(dateHelper()).not.be.ok;
-        })
-    })
-})
+        });
+    });
+});
